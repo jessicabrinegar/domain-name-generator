8b714d672f223cf09b8d3cc9b50d95341df36f8e
jessicabrinegar <jessicabrinegar@outlook.com>
2023-01-05 00:08:57 +0000

    just some aesthetic tweaks

--- src/app.js
+++ src/app.js
@@ -5,17 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function(pronoun, adj, noun, ext) {
-  pronoun = ["your", "my"];
-  adj = ["tempestuous", "amusing", "provocative"];
-  noun = ["laughter", "emotion", "code"];
-  ext = [".com", ".net", ".io"];
+window.onload = function(pronouns, adjectives, nouns, extensions) {
+  pronouns = ["your", "my"];
+  adjectives = ["tempestuous", "amusing", "provocative"];
+  nouns = ["laughter", "emotion", "code"];
+  extensions = [".com", ".net", ".io"];
   const result = [];
-  for (const pro of pronoun) {
-    for (const ad of adj) {
-      for (const no of noun)
-        for (const ex of ext) {
-          result.push(pro + ad + no + ex);
+  for (const pronoun of pronouns) {
+    for (const adj of adjectives) {
+      for (const noun of nouns)
+        for (const ext of extensions) {
+          result.push(pronoun + adj + noun + ext);
         }
     }
   }
